c184d3125b1e72f876ebf2e89d68a5ceab78f3fd
JoseLuiz <zzeris@gmail.com>
2021-10-27 10:02:06 -0300

    Add New transaction modal

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Alugel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 2000,
+          createdAt: new Date('2021-03-13 10:00:00')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
